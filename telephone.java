a64d781eb226b0f3b76c1774acf4d2c0a51499aa
Carson-Marden12 <mardenboy55@gmail.com>
2026-06-03 10:22:42 -0500

    called function

--- telephone.java
+++ telephone.java
@@ -1,6 +1,10 @@
 public class telephone {
     public static void main(String[] args) {
         int j = 10;
+        j = addNum(j);
+
+
+    System.out.println(j);
     }
     public static int addNum(int a) {
         return a + 1;
